68e677fe67f6384e9f5ef0168ba13187b62600f2
Benjamin Knobloch <54300148+beknobloch@users.noreply.github.com>
2024-06-24 11:09:18 -0600

    Create Colony.java

---
+++ Dominion/src/component/Colony.java
@@ -0,0 +1,14 @@
+package component;
+
+public class Colony extends Victory {
+
+    @Override
+    public int cost() { return 11; }
+
+    @Override
+    public int victory_points() { return 10; }
+
+    @Override
+    public String toString() { return "Province"; }
+
+}
