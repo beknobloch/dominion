3d623fbb4dcf1b1b72d3899fbc654eec5b83c20e
Benjamin Knobloch <54300148+beknobloch@users.noreply.github.com>
2024-06-24 11:10:53 -0600

    Create Platinum.java

---
+++ Dominion/src/component/Platinum.java
@@ -0,0 +1,14 @@
+package component;
+
+public class Platinum extends Treasure {
+
+    @Override
+    public int cost() { return 9; }
+
+    @Override
+    public int value() { return 5; }
+
+    @Override
+    public String toString() { return "Platinum"; }
+
+}
