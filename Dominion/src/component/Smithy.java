f90c12dec1fcfde32fce15b2bbce3e886d3a3f10
Benjamin Knobloch <54300148+beknobloch@users.noreply.github.com>
2024-06-24 10:09:23 -0600

    Create Smithy.java

---
+++ Dominion/src/component/Smithy.java
@@ -0,0 +1,14 @@
+package component;
+
+public class Smithy extends Action {
+
+    @Override
+    public int cost() { return 4; }
+
+    @Override
+    public int cards() { return 3; }
+
+    @Override
+    public String toString() { return "Smithy"; }
+
+}
