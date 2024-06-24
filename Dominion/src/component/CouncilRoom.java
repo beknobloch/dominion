a873152974e76b443bb5dd93582f362a54591cd1
Benjamin Knobloch <54300148+beknobloch@users.noreply.github.com>
2024-06-24 10:29:54 -0600

    Create CouncilRoom.java

---
+++ Dominion/src/component/CouncilRoom.java
@@ -0,0 +1,29 @@
+package components;
+
+import java.util.ArrayList;
+
+public class CouncilRoom extends Action {
+
+  @Override
+  public int cost() { return 5; }
+
+  @Override
+  public int cards() { return 4; }
+  @Override
+  public int buys() { return 1; }
+
+  @Override
+  public void effect(Player player, ArrayList<Player> otherPlayers)
+  {
+
+    for (Player p : otherPlayers)
+      p.draw(1);
+    
+  }
+
+  @Override
+  public String toString() { return "Council Room"; }
+  @Override
+  public String description() { return "Each other player draws a card."; }
+  
+}
