28a384c022d3ac6b9f95476db3f0d050e49a678a
Benjamin Knobloch <54300148+beknobloch@users.noreply.github.com>
2024-06-24 10:23:02 -0600

    Update Village.java

--- Dominion/src/component/Village.java
+++ Dominion/src/component/Village.java
@@ -9,10 +9,6 @@ public class Village extends Action {
     public int actions() { return 2; }
     @Override
     public int cards() { return 1; }
-    @Override
-    public int coin() { return 0; }
-    @Override
-    public int buys() { return 0; }
 
     @Override
     public String toString() { return "Village"; }
