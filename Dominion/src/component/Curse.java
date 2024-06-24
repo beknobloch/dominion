c45be6f87aa335f67c864f2feec84ea5cd24c022
Benjamin Knobloch <54300148+beknobloch@users.noreply.github.com>
2024-06-24 07:59:22 -0600

    Update Curse.java

--- Dominion/src/component/Curse.java
+++ Dominion/src/component/Curse.java
@@ -14,6 +14,8 @@ public class Curse extends Card {
 
     @Override
     public String toString() { return "Curse"; }
+    @Override
+    public String typeToString() { return ""; }
 
     @Override
     public String description() {
